119058588f7a2b3401e9fb8a4188db73576fdc93
RazuAhmad <abdurrahmanrazu2000@gmail.com>
2023-06-28 04:41:52 +0600

    jwt implementation running

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app =express();
 const port =process.env.PORT || 5000;
 
+// authentication related imports::
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 // middleware
 app.use(cors());
 app.use(express.json());
+
 
 // Mongodb userName & Password are here...
 // user: donationSystemDB
@@ -17,6 +22,9 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://donationSystemDB:AOg5u0MNSlYya4v2@cluster0.zw95lnu.mongodb.net/?retryWrites=true&w=majority`;
+
+// Secret key for JWT
+const secretKey = 'razus super secret key';
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -34,7 +42,7 @@ async function run() {
 
         const totalDonators=client.db('allDonatorsDB').collection('allDonatorsCollection');
 
-        
+        const userAuthentication=client.db('userAuthDB').collection('userCredentialsCollection')
 
     // Read or Find/Query Method.::::
 
@@ -54,7 +62,7 @@ async function run() {
             
         })
 
-    // Update method done here...
+    // Find method for searching specific user done...
     app.get('/donators/:id',async(req,res)=>{
         const id=req.params.id;
         
@@ -82,6 +90,64 @@ async function run() {
         console.log("updated user",updatedUser);
         res.json(result)
     })
+    /*Donation information related database works ends here----------------------------------------------------------------*/ 
+
+
+    /*User Authentication related works starts from here */
+    /*------------------------------------------------------------*/
+
+    // User Registration starts from here::::
+    app.post('/register', (req, res) => {
+        const { name,email, password } = req.body;
+    console.log(req.body);
+        // Hash the password using bcrypt
+        bcrypt.hash(password, 10, (err, hashedPassword) => {
+          if (err) {
+            res.status(500).json({ error: 'Error hashing password' });
+          } else {
+            // Save the user in the MongoDB collection
+            userAuthentication.insertOne({ name, password: hashedPassword,email }, (err) => {
+              if (err) {
+                res.status(500).json({ error: 'Error registering user' });
+              } else {
+                console.log(res.status(201).json({ message: 'User registered successfully' }));;
+              }
+            });
+          }
+        });
+      });
+
+    //   User login:::::::::::::::::::::::
+    app.post('/login', (req, res) => {
+        const { email, password } = req.body;
+    userAuthentication.findOne({ email }, (err, user));
+        // Find the user in the MongoDB collection
+        userAuthentication.findOne({ email }, (err, user) => {
+          if (err) {
+            res.status(500).json({ error: 'Error finding user' });
+          } else if (!user) {
+            res.status(401).json({ error: 'Invalid email or password' });
+          } else {
+            // Compare the provided password with the hashed password in the database using bcrypt
+            bcrypt.compare(password, user.password, (err, result) => {
+              if (err) {
+                res.status(500).json({ error: 'Error comparing passwords' });
+                console.log(err);
+              } else if (!result) {
+                res.status(401).json({ error: 'Invalid email or password' });
+                
+              } else {
+                // Generate a JWT token
+                const token = jwt.sign({ email }, secretKey, { expiresIn: '1h' });
+                res.status(200).json({ token });
+                console.log(token);
+                
+              }
+            });
+          }
+        });
+      });
+    
 
     
     } finally {
